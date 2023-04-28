23afac2eae76ecb57dea9db434a6daef0e2e6245
ruman-abdi <rahrumanabdii@gmail.com>
2023-04-27 21:38:18 -0700

    add some media query

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,7 +8,6 @@ export const Navbar = () => {
     <div className="navbar">
       <div className="links">
         <Link to="/"> Shop </Link>
-      
         <Link to="/cart">
           <ShoppingCart size={32} />
         </Link>
